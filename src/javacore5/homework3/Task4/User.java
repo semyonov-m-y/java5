38b4f5b674cce9850440d870913ba87c89702f1e
semyonov-m-y <semyonov-m-y@mail.ru>
2016-11-12 20:05:30 +0300

    Homework3. Task3. Attempt2

--- src/javacore5/homework3/Task4/User.java
+++ src/javacore5/homework3/Task4/User.java
@@ -8,58 +8,9 @@ public class User {
 	String companyName;
 	int salary;
 	String currency;
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getBalance() {
-		return balance;
-	}
-	
-	public void setBalance(int balance) {
-		this.balance = balance;
-	}
-	
-	public int getMonthOfEmployment() {
-		return monthsOfEmployment;
-	}
-	
-	public void setMonthOfEmployment(int monthOfEmployment) {
-		this.monthsOfEmployment = monthOfEmployment;
-	}
-	
-	public String getCompanyName() {
-		return companyName;
-	}
-	
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
-	
-	public int getSalary() {
-		return salary;
-	}
-	
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-	
-	public String getCurrency() {
-		return currency;
-	}
-	
-	public void setCurrency(String currency) {
-		this.currency = currency;
-	}
 
 	public User(String name, int balance, int monthOfEmployment,
 				String companyName, int salary, String currency) {
-		super();
 		this.name = name;
 		this.balance = balance;
 		this.monthsOfEmployment = monthOfEmployment;
@@ -73,11 +24,11 @@ public class User {
 	}
 	
 	public double withdraw(int summ) {
-			if (summ < 1000) {
-				return summ + (summ*0.05);
-			} else {
-				return summ + (summ*0.1);
-			}
+		if (summ < 1000) {
+			return summ + (summ*0.05);
+		} else {
+			return summ + (summ*0.1);
+		}
 	}
 	
 	public int companyNameLength(String companyName) {
@@ -90,5 +41,53 @@ public class User {
 	public int monthsIncreaser(int addMonth) {
 		monthsOfEmployment += addMonth;
 		return monthsOfEmployment;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public int getMonthOfEmployment() {
+		return monthsOfEmployment;
+	}
+
+	public void setMonthOfEmployment(int monthOfEmployment) {
+		this.monthsOfEmployment = monthOfEmployment;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
 	}
 }

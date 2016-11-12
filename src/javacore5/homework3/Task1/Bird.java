2482676e696921035e38ff0c3bbd6397ae69dba6
semyonov-m-y <semyonov-m-y@mail.ru>
2016-11-12 13:46:21 +0300

    Homework3. Task1

---
+++ src/javacore5/homework3/Task1/Bird.java
@@ -0,0 +1,18 @@
+package javacore5.homework3.Task1;
+
+public class Bird {
+	
+	public static void main(String[] args) {
+		sing("I am walking");
+		sing("I am flying");
+		sing("I am singing");
+		sing("I am Bird");
+	}
+	
+	public static void sing(String s) {
+		System.out.println(s + "");
+		System.out.println(s);
+		System.out.println(s);
+		System.out.println(s);
+	}
+}

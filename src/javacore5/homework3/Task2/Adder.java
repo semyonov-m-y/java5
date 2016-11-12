0349cbd115dfbac0c8330d6408c825eb116996ae
semyonov-m-y <semyonov-m-y@mail.ru>
2016-11-12 13:33:44 +0300

    Homework2. Task2

---
+++ src/javacore5/homework3/Task2/Adder.java
@@ -0,0 +1,12 @@
+package javacore5.homework3.Task2;
+
+public class Adder extends Arithmetic {
+	
+	public boolean check(Integer a, Integer b) {
+		if(a >= b) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ src/javacore5/homework3/Task2/Arithmetic.java
@@ -0,0 +1,10 @@
+package javacore5.homework3.Task2;
+
+public class Arithmetic {
+
+	public int add(Integer a, Integer b) {
+		int summ = a + b;	
+		return summ;	
+	}
+
+}

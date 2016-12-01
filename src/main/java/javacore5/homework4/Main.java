efa79b78d0f3a00fe23c10651d1364cb4d15a65d
semyonov-m-y <semyonov-m-y@mail.ru>
2016-12-01 20:12:50 +0300

    HW4

--- src/main/java/javacore5/homework4/Main.java
+++ src/main/java/javacore5/homework4/Main.java
@@ -5,22 +5,22 @@ public class Main extends BankSystemImpl {
     public static void main(String[] args) {
         BankSystemImpl bsImpl = new BankSystemImpl();
 
-        Bank usBank1 = new USBank(0l, "USA", Currency.EUR, 100, 7000.0, 13l, 1000000000l);
+        Bank usBank1 = new USBank(0l, "USA", Currency.EUR, 100, 7000.0, 13l, 1000000000L);
         User user1 = new User(10l, "Max", 100000.0, 9, "SBT", 100000, usBank1);
 
-        Bank usBank2 = new USBank(1, "USA", Currency.USD, 1000, 6000.0, 13l, 1000000000l);
+        Bank usBank2 = new USBank(1, "USA", Currency.USD, 1000, 6000.0, 13l, 1000000000L);
         User user2 = new User(11l, "Alex", 100000.0, 10, "Diksi", 100000, usBank2);
 
-        Bank euBank1 = new EUBank(2, "Germany", Currency.EUR, 100, 5000.0, 13l, 1000000000l);
+        Bank euBank1 = new EUBank(2, "Germany", Currency.EUR, 100, 5000.0, 13l, 1000000000L);
         User user3 = new User(12l, "Anna", 100000.0, 11, "Sberbank", 100000, euBank1);
 
-        Bank euBank2 = new EUBank(3, "France", Currency.USD, 1000, 4000.0, 13l, 1000000000l);
+        Bank euBank2 = new EUBank(3, "France", Currency.USD, 1000, 4000.0, 13l, 1000000000L);
         User user4 = new User(1l, "Elena", 100000.0, 12, "Luxoft", 100000, euBank2);
 
-        Bank chinaBank1 = new ChinaBank(4, "China", Currency.EUR, 100, 3000.0, 13l, 1000000000l);
+        Bank chinaBank1 = new ChinaBank(4, "China", Currency.EUR, 100, 3000.0, 13l, 1000000000L);
         User user5 = new User(2l, "Bob", 100000.0, 1, "Applana", 100000, chinaBank1);
 
-        Bank chinaBank2 = new ChinaBank(5, "China", Currency.USD, 1000, 2000.0, 13l, 1000000000l);
+        Bank chinaBank2 = new ChinaBank(5, "China", Currency.USD, 1000, 2000.0, 13l, 1000000000L);
         User user6 = new User(3l, "Piter", 100000.0, 2, "5_Seasons", 100000, chinaBank2);
 
         bsImpl.withdrawOfUser(user1, 100);

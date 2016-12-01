e28833f5adfa3e7e8b3af6ffd6fb5072e61834e8
semyonov-m-y <semyonov-m-y@mail.ru>
2016-12-01 19:30:16 +0300

    HW2

--- src/main/java/javacore5/homework2/Task2.java
+++ src/main/java/javacore5/homework2/Task2.java
@@ -2,22 +2,22 @@ package main.java.javacore5.homework2;
 
 public class Task2 {
 
-	public static void takeYourMoneyOnce(double startBalance, double withdrawal) {
-		double comission = withdrawal*0.05;
-		double withdrawalAndComission = withdrawal + comission;
-		double balance = startBalance - withdrawalAndComission;
+    public static void takeYourMoneyOnce(double startBalance, double withdrawal) {
+        double comission = withdrawal * 0.05;
+        double withdrawalAndComission = withdrawal + comission;
+        double balance = startBalance - withdrawalAndComission;
 
-		if (startBalance >= withdrawalAndComission) {
-			System.out.println("OK" + " " + comission + " " + balance);
-		} else {
-			System.out.println("NO");
-		}
-	}
+        if (startBalance >= withdrawalAndComission) {
+            System.out.println("OK" + " " + comission + " " + balance);
+        } else {
+            System.out.println("NO");
+        }
+    }
 
-	public static void main(String[] args) {
-		takeYourMoneyOnce(100.0, 80);
-		takeYourMoneyOnce(100.0, 99);
-		takeYourMoneyOnce(100.0, 10);
-		takeYourMoneyOnce(10.0, 5);
-	}
+    public static void main(String[] args) {
+        takeYourMoneyOnce(100.0, 80);
+        takeYourMoneyOnce(100.0, 99);
+        takeYourMoneyOnce(100.0, 10);
+        takeYourMoneyOnce(10.0, 5);
+    }
 }
--- src/main/java/javacore5/homework2/Task4.java
+++ src/main/java/javacore5/homework2/Task4.java
@@ -2,25 +2,25 @@ package main.java.javacore5.homework2;
 
 public class Task4 {
 
-	static int[] balances = {1200, 250, 2000, 500, 3200};
-	static String[] ownerNames = {"Jane", "Ann", "Jake", "Oww", "Lane"};
+    static int[] balances = {1200, 250, 2000, 500, 3200};
+    static String[] ownerNames = {"Jane", "Ann", "Jake", "Oww", "Lane"};
 
-	public static void putMoneyOfExactOwner(String ownerName, double fund) {
-		for (int i = 0; i < ownerNames.length; i++) {
-			if (ownerNames[i].equals(ownerName)) {
-				double balance = balances[i];
-				double newBalance = balance + fund;
+    public static void putMoneyOfExactOwner(String ownerName, double fund) {
+        for (int i = 0; i < ownerNames.length; i++) {
+            if (ownerNames[i].equals(ownerName)) {
+                double balance = balances[i];
+                double newBalance = balance + fund;
 
-				System.out.println(ownerName + " " + newBalance);
-			}
-		}
-	}
+                System.out.println(ownerName + " " + newBalance);
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		putMoneyOfExactOwner("Jane", 100);
-		putMoneyOfExactOwner("Ann", 1000);
-		putMoneyOfExactOwner("Jake", 100);
-		putMoneyOfExactOwner("Oww", 100);
-		putMoneyOfExactOwner("Lane", 100);
-	}
+    public static void main(String[] args) {
+        putMoneyOfExactOwner("Jane", 100);
+        putMoneyOfExactOwner("Ann", 1000);
+        putMoneyOfExactOwner("Jake", 100);
+        putMoneyOfExactOwner("Oww", 100);
+        putMoneyOfExactOwner("Lane", 100);
+    }
 }

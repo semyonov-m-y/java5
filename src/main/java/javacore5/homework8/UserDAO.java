b803ceb29c73d82b29351391ed704395d9a3c316
semyonov-m-y <semyonov-m-y@mail.ru>
2016-12-29 21:19:04 +0300

    Update UserDAO.java

--- src/main/java/javacore5/homework8/UserDAO.java
+++ src/main/java/javacore5/homework8/UserDAO.java
@@ -1,56 +1,17 @@
 package javacore5.homework8;
 
-import java.util.List;
+import java.util.Iterator;
 
 public class UserDAO extends AbstractDAOImpl<User> {
 
-    @Override
-    public User save(User user) {
-        base.add(user);
-        System.out.println(user.getName() + " was saved in DB");
-
-        return user;
+    public UserDAO() {
+        save(new User(1L, "Slava"));
+        save(new User(2L, "Lesha"));
+        save(new User(3L, "Vasya"));
     }
 
     @Override
-    public void delete(User user) {
-        for (int i = 0; i < base.size(); i++) {
-            if (base.get(i).equals(user)) {
-                base.remove(i);
-                System.out.println(user.getName() + " was removed from DB");
-            }
-        }
-    }
-
-    @Override
-    public void deleteAll(List<User> user) {
-        user.clear();
-        System.out.println("All elements were removed from DB");
-    }
-
-    @Override
-    public void saveAll(List<User> user) {
-        base.addAll(user);
-        System.out.println("All elements from list were added in DB");
-    }
-
-    @Override
-    public List<User> getList() {
-        for (int i = 0; i < base.size(); i++) {
-            System.out.println(base.get(i));
-        }
-
-        return base;
-    }
-
-    @Override
-    public void deleteById(long id) {
-        base.remove(id);
-        System.out.println("Element with id " + id + " was removed from DB");
-    }
-
-    @Override
-    public User get(long id) {
+    public User getById(long id) {
         for (User user : base) {
             if (user.getId() == id) {
                 return user;
@@ -58,5 +19,17 @@ public class UserDAO extends AbstractDAOImpl<User> {
         }
 
         return null;
+    }
+
+    @Override
+    public void deleteById(long id) {
+        Iterator<User> iterator = base.iterator();
+
+        while (iterator.hasNext()) {
+            User item = iterator.next();
+            if (item.getId() == id) {
+                iterator.remove();
+            }
+        }
     }
 }

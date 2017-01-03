1af9f1b3b159ed5295a7fc9582d53f0c80d41e85
semyonov-m-y <semyonov-m-y@mail.ru>
2017-01-03 19:07:17 +0300

    Update UserDAO.java

--- src/main/java/javacore5/homework8/UserDAO.java
+++ src/main/java/javacore5/homework8/UserDAO.java
@@ -1,26 +1,14 @@
 package javacore5.homework8;
 
-import java.util.List;
-import java.util.Optional;
-
 public class UserDAO extends AbstractDAOImpl<User> {
 
     @Override
-    public Optional<User> getById(long id) {
-        Optional<User> user = base.stream().filter(p -> p.getId() == id).findAny();
-        return user;
+    public User getById(long id) {
+        return getList().stream().filter(user -> user.getId() == id).findAny().orElse(null);
     }
 
     @Override
     public void deleteById(long id) {
-        base.removeIf(ourList -> ourList.getId() == id);
-    }
-
-    public List<User> getBase() {
-        return base;
-    }
-
-    public void setBase(List<User> base) {
-        this.base = base;
+        getList().removeIf(user -> user.getId() == id);
     }
 }

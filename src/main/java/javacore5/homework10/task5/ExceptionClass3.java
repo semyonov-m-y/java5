ebb8cae1b39218a2f90c079345292c72f890cc1f
semyonov-m-y <semyonov-m-y@mail.ru>
2017-01-17 21:42:51 +0300

    Add files via upload

---
+++ src/main/java/javacore5/homework10/task5/ExceptionClass3.java
@@ -0,0 +1,14 @@
+package javacore5.homework10.task5;
+
+public class ExceptionClass3 extends Exception {
+    public ExceptionClass3(String string) {
+    }
+
+    public static void ffunction() throws ExceptionClass2 {
+        gfunction();
+    }
+
+    public static void gfunction() throws ExceptionClass2 {
+        throw new ExceptionClass2("ExceptionClass2");
+    }
+}

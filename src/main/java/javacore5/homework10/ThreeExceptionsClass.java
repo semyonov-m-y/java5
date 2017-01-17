4ddc7da7c0614c883678376fe2c0ccdc736f475d
semyonov-m-y <semyonov-m-y@mail.ru>
2017-01-17 21:42:23 +0300

    Delete ThreeExceptionsClass.java

--- src/main/java/javacore5/homework10/ThreeExceptionsClass.java
+++
@@ -1,8 +0,0 @@
-package main.java.homeworks.homework10;
-
-public class ThreeExceptionsClass extends Exception {
-
-	public static void threeExceptionsMethod() throws MyClass1, MyClass2, MyClass3 {
-		
-	}
-}

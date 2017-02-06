ad5988072d7a02b97b7b94b53426dfff7070c510
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-06 20:00:57 +0300

    Update ReplacerClass.java

--- src/main/java/javacore5/homework11/ReplacerClass.java
+++ src/main/java/javacore5/homework11/ReplacerClass.java
@@ -1,95 +1,42 @@
 package javacore5.homework11;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 
 public class ReplacerClass {
 
-    static FileWriter writer = null;
     public static String replacer(Map<String, String> map) throws IOException {
-        String var;
-        String string = "Max";
-        String replacedString = null;
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(MainClass.file));
+								List<String> lines = Files.readAllLines(Paths.get("src\\main\\java\\javacore5\\homework11\\task.txt"),
+																StandardCharsets.UTF_8);
 
-            try {
-                while ((var = reader.readLine()) != null) {
-                    map.put(var, string);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+								String stringFromFile = "";
+								for(String line : lines) {
+												stringFromFile = line.concat(stringFromFile);
+								}
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
-        }
+								String replacedString = "";
+								for(Map.Entry<String, String> pair : map.entrySet()){
+												replacedString = stringFromFile.replaceAll(pair.getKey(), pair.getValue());
+								}
 
-        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
-
-        while (iterator.hasNext()) {
-            Map.Entry<String, String> pair = iterator.next();
-            String value = pair.getValue();
-            replacedString = value;
-        }
-
-        return replacedString;
+								return replacedString;
     }
 
     public static File fileContentReplacer(Map<String, String> map) throws IOException {
-        String string = "Max1";
+        WriterClass.rewriteFile(replacer(map));
 
-        replacer(map);
-
-        try {
-            writer = new FileWriter(MainClass.file);
-            writer.write(string);
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
-
-        PrintClass.printFile(MainClass.file);
-
-        return MainClass.file;
+        return MainClass.TEXT_FILE;
     }
 
     public static File fileContentMerger(Map<String, String> map) throws IOException {
-        String string = "Max2";
-        String string1 = "Max3";
+        WriterClass.appendStringToFile(replacer(map));
 
-        replacer(map);
-
-        try {
-            writer = new FileWriter(MainClass.file);
-            writer.write(string);
-            writer.append(string1);
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
-
-        PrintClass.printFile(MainClass.file);
-
-        return MainClass.file;
+        return MainClass.TEXT_FILE;
     }
+
 }

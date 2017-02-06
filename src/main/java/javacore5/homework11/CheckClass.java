8fdb415f4ab5a99a21161ba0af44201856c63dfa
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-06 19:59:39 +0300

    Update CheckClass.java

--- src/main/java/javacore5/homework11/CheckClass.java
+++ src/main/java/javacore5/homework11/CheckClass.java
@@ -3,55 +3,56 @@ package javacore5.homework11;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CheckClass {
 
-    static String var;
-    static int wordsCounter = 0;
     public static int checkWord1(String word) throws IOException {
-        BufferedReader reader = null;
+        int wordsCounter1 = 0;
+        List<String> lines = Files.readAllLines(Paths.get("src\\main\\java\\javacore5\\homework11\\task.txt"),
+                StandardCharsets.UTF_8);
 
-        try {
-            reader = new BufferedReader(new FileReader(MainClass.file));
-
-            try {
-                while ((var = reader.readLine()) != null) {
-                    if (word.contains(var)) {
-                        wordsCounter++;
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                reader.close();
+        for (String line : lines) {
+            line.split(word);
+            if (line.split(word) != null) {
+                wordsCounter1++;
             }
         }
 
-        return wordsCounter;
+        return wordsCounter1;
     }
 
     public static int checkWord2(String word) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(MainClass.file));
+        int wordsCounter2 = 0;
+    	/*ArrayList<String> list = new ArrayList<>();
+    	Scanner file = new Scanner(new File("src\\main\\java\\javacore5\\homework11\\task.txt"));
+    	while (file.hasNext()) {
+    		list.add(file.nextLine());
+    	}
 
-        try {
-            while ((var = reader.readLine()) != null) {
-                if (word.contains(var)) {
-                    wordsCounter++;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
-        }
+    	for (int i = 0; i < list.size(); i++) {
+    		if (word.equals(list.get(i))) {
+    			System.out.println(list.get(i));
+    			wordsCounter2++;
+    		}
+    	}
 
-        return wordsCounter;
+    	for (String line : list) {
+        	line.split(word);
+            if (line.toString().contains(word)) {
+                wordsCounter2++;
+            }
+        }*/
+        Pattern p = Pattern.compile(word, Pattern.UNICODE_CASE| Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(word);
+        while (m.find()) wordsCounter2++;
+
+        return wordsCounter2;
     }
 }

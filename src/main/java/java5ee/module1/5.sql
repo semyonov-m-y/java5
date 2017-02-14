8e036fba87dfd8f21933a2d7bab01c2e6bc2e357
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-14 21:10:36 +0300

    Update 5.sql

--- src/main/java/java5ee/module1/5.sql
+++ src/main/java/java5ee/module1/5.sql
@@ -1,1 +1,2 @@
-SELECT  cost, MIN(SUM(salary)) FROM sqldatabase.customers LEFT JOIN sqldatabase.projects ON customers.id = projects.customer_id GROUP BY cost;
+SELECT MIN(cost) FROM sqldatabase.projects WHERE company LIKE 'Luxoft';
+SELECT MIN(cost) FROM sqldatabase.projects WHERE company LIKE 'MTS';

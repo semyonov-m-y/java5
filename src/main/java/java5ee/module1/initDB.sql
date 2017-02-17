c73385b69aba14bad044487a59dc467eff95e6df
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-17 21:27:32 +0300

    Update initDB.sql

--- src/main/java/java5ee/module1/initDB.sql
+++ src/main/java/java5ee/module1/initDB.sql
@@ -5,43 +5,31 @@ last_name  VARCHAR(200)  NOT NULL,
 email  VARCHAR(200)  NOT NULL
 );
 
-ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS developers (
 id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name  VARCHAR(50)  NOT NULL,
 last_name  VARCHAR(200)  NOT NULL,
-email  VARCHAR(200)  NOT NULL
+email  VARCHAR(200)  NOT NULL,
+project_id  INT
 );
-
-ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS companies (
 id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
 company VARCHAR(200)  NOT NULL,
-developer_id  INT,
-customer_id  INT
 );
-
-ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS projects (
 id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
 project  VARCHAR(200)  NOT NULL,
 company VARCHAR(200)  NOT NULL,
-developer_id  INT,
 customer_id  INT
 );
-
-ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS skills (
 id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
 skill VARCHAR(200)  NOT NULL,
 developer_id  INT
 );
-
-ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS references (
 customer_id INT NOT NULL,
@@ -52,5 +40,3 @@ FOREIGN KEY (developer_id) REFERENCES developers (id),
 
 UNIQUE (customer_id, developer_id)
 );
-
-ENGINE = InnoDB;

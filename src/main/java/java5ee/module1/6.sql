5923d0c8ba894182bb5a5b37bd07b09b7f2f150a
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-17 21:25:49 +0300

    Update 6.sql

--- src/main/java/java5ee/module1/6.sql
+++ src/main/java/java5ee/module1/6.sql
@@ -1,3 +1,3 @@
 SELECT AVG(salary), projects.project, projects.cost FROM developers
-LEFT JOIN projects ON developers.id = projects.developer_id
+LEFT JOIN projects ON developers.project_id = projects.id
 GROUP BY project ORDER BY cost LIMIT 1;

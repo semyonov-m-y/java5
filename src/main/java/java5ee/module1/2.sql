27cb223b25b77aeb16609b789121be4028e74e78
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-19 08:22:19 +0300

    Update 2.sql

--- src/main/java/java5ee/module1/2.sql
+++ src/main/java/java5ee/module1/2.sql
@@ -1,3 +1,3 @@
-SELECT SUM(salary), projects.project FROM developers 
+SELECT SUM(salary), projects.project_name FROM developers 
 LEFT JOIN projects ON developers.project_id = projects.id
-GROUP BY project ORDER BY SUM(salary) DESC LIMIT 1;
+GROUP BY project_name ORDER BY SUM(salary) DESC LIMIT 1;

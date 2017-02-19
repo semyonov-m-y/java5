5553d7dce55f26d91567731bf4c57c1eab7d0902
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-19 08:24:05 +0300

    Update initDB.sql

--- src/main/java/java5ee/module1/initDB.sql
+++ src/main/java/java5ee/module1/initDB.sql
@@ -10,33 +10,32 @@ id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name  VARCHAR(50)  NOT NULL,
 last_name  VARCHAR(200)  NOT NULL,
 email  VARCHAR(200)  NOT NULL,
-project_id  INT
+project_id  INT,
+FOREIGN KEY (project_id) REFERENCES projects (id)
 );
 
 CREATE TABLE IF NOT EXISTS companies (
 id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-company VARCHAR(200)  NOT NULL,
+company_name VARCHAR(200)  NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS projects (
 id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-project  VARCHAR(200)  NOT NULL,
-company VARCHAR(200)  NOT NULL,
-customer_id  INT
+project_name  VARCHAR(200)  NOT NULL,
+company_id INT  NOT NULL,
+customer_id  INT NOT NULL,
+FOREIGN KEY (customer_id) REFERENCES customers (id),
+FOREIGN KEY (company_id) REFERENCES companies(id)
 );
 
 CREATE TABLE IF NOT EXISTS skills (
 id  INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-skill VARCHAR(200)  NOT NULL,
-developer_id  INT
+skill VARCHAR(200)  NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS references (
-customer_id INT NOT NULL,
-developer_id INT NOT NULL,
-
-FOREIGN KEY (customer_id) REFERENCES customers (id),
+CREATE TABLE IF NOT EXISTS developers_skills (
+developer_id INT  NOT NULL,
+skill_id  INT NOT NULL,
 FOREIGN KEY (developer_id) REFERENCES developers (id),
-
-UNIQUE (customer_id, developer_id)
+FOREIGN KEY (skill_id) REFERENCES skills (id)
 );

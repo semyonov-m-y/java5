8f3fdad9e2f4b6239366a95824ae4f70790de84a
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-20 19:40:22 +0300

    Update 3.sql

--- src/main/java/java5ee/module1/3.sql
+++ src/main/java/java5ee/module1/3.sql
@@ -1,1 +1,3 @@
-SELECT SUM(salary) FROM developers;
+SELECT SUM(salary) FROM javaee.developers
+LEFT JOIN javaee.developers_skills ON javaee.developers_skills.developer_id = javaee.developers.id 
+WHERE skill_id = 2 GROUP BY skill_id;

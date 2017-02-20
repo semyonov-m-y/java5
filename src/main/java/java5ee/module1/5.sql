33e3dc094b0d7a3642bb0aea9467eb01ae244f5f
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-20 19:40:01 +0300

    Update 5.sql

--- src/main/java/java5ee/module1/5.sql
+++ src/main/java/java5ee/module1/5.sql
@@ -1,2 +1,2 @@
-SELECT MIN(cost) FROM projects WHERE company_id = 1;
-SELECT MIN(cost) FROM projects WHERE company_id = 2;
+SELECT MIN(cost) FROM javaee.projects WHERE company_id = 1 GROUP BY customer_id;
+SELECT MIN(cost) FROM javaee.projects WHERE company_id = 2 GROUP BY customer_id;

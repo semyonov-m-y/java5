c0c1cd508b4b90f88b2ec4110b382fe3d5ac8aa4
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-21 20:06:42 +0300

    Update 5.sql

--- src/main/java/java5ee/module1/5.sql
+++ src/main/java/java5ee/module1/5.sql
@@ -1,9 +1,9 @@
-SELECT SUM(cost), javaee.companies.company_name, javaee.projects.customer_id FROM javaee.projects
-LEFT JOIN javaee.companies ON javaee.companies.id = javaee.projects.company_id
+SELECT SUM(cost), companies.company_name, projects.customer_id FROM projects
+LEFT JOIN companies ON companies.id = projects.company_id
 WHERE company_name LIKE "Luxoft"
 GROUP BY customer_id ORDER BY SUM(cost) LIMIT 1;
 
-SELECT SUM(cost), javaee.companies.company_name, javaee.projects.customer_id FROM javaee.projects
-LEFT JOIN javaee.companies ON javaee.companies.id = javaee.projects.company_id
+SELECT SUM(cost), companies.company_name, projects.customer_id FROM projects
+LEFT JOIN companies ON companies.id = projects.company_id
 WHERE company_name LIKE "MTS"
 GROUP BY customer_id ORDER BY SUM(cost) LIMIT 1;

89de7f6dae2aa93cf026ffe3e9bd9d8377e67a64
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-21 20:05:53 +0300

    Update 5.sql

--- src/main/java/java5ee/module1/5.sql
+++ src/main/java/java5ee/module1/5.sql
@@ -1,2 +1,9 @@
-SELECT MIN(cost) FROM javaee.projects WHERE company_id = 1 GROUP BY customer_id;
-SELECT MIN(cost) FROM javaee.projects WHERE company_id = 2 GROUP BY customer_id;
+SELECT SUM(cost), javaee.companies.company_name, javaee.projects.customer_id FROM javaee.projects
+LEFT JOIN javaee.companies ON javaee.companies.id = javaee.projects.company_id
+WHERE company_name LIKE "Luxoft"
+GROUP BY customer_id ORDER BY SUM(cost) LIMIT 1;
+
+SELECT SUM(cost), javaee.companies.company_name, javaee.projects.customer_id FROM javaee.projects
+LEFT JOIN javaee.companies ON javaee.companies.id = javaee.projects.company_id
+WHERE company_name LIKE "MTS"
+GROUP BY customer_id ORDER BY SUM(cost) LIMIT 1;

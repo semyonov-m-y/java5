7941032ef9f9af335480903ca2d7c8a3feb4a59e
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-24 08:42:24 +0300

    Update 5.sql

--- src/main/java/java5ee/module1/5.sql
+++ src/main/java/java5ee/module1/5.sql
@@ -1,9 +1,6 @@
-SELECT SUM(cost), companies.company_name, projects.customer_id FROM projects
+SELECT MIN(profit), company_name, customer_id FROM (
+SELECT SUM(cost) AS profit, companies.company_name, projects.customer_id 
+FROM projects
 LEFT JOIN companies ON companies.id = projects.company_id
-WHERE company_name LIKE "Luxoft"
-GROUP BY customer_id ORDER BY SUM(cost) LIMIT 1;
-
-SELECT SUM(cost), companies.company_name, projects.customer_id FROM projects
-LEFT JOIN companies ON companies.id = projects.company_id
-WHERE company_name LIKE "MTS"
-GROUP BY customer_id ORDER BY SUM(cost) LIMIT 1;
+GROUP BY customer_id, company_id ORDER BY SUM(cost)) AS item
+GROUP BY company_name;

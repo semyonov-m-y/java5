a6e12335762ead40a3efe9fac4121d4539c6b5e2
semyonov-m-y <semyonov-m-y@mail.ru>
2017-02-24 08:44:45 +0300

    Update 5.sql

--- src/main/java/java5ee/module1/5.sql
+++ src/main/java/java5ee/module1/5.sql
@@ -2,5 +2,5 @@ SELECT MIN(profit), company_name, customer_id FROM (
 SELECT SUM(cost) AS profit, companies.company_name, projects.customer_id 
 FROM projects
 LEFT JOIN companies ON companies.id = projects.company_id
-GROUP BY customer_id, company_id ORDER BY SUM(cost)) AS item
+GROUP BY customer_id, company_id) AS item
 GROUP BY company_name;
